c7780457bb4a2f23c35547f9669b7d01b1b0132f
Garrett Pilgrim <pilgrimgarrett@gmail.com>
2021-08-21 14:37:13 -0500

    updates to style and index as well as typography changes

--- src/components/Section/section.style.js
+++ src/components/Section/section.style.js
@@ -3,10 +3,15 @@ import styled from "styled-components";
 const SectionStyle = styled.section`
   display: grid;
   grid-template-columns: minmax(10px, 1fr) auto minmax(10px, 1fr);
-  justify-items: stretch;
 
   * {
     grid-column: 2 / 3;
+  }
+
+  h2 {
+    justify-self: start;
+    margin: 8rem 0 0 0;
+    font-size: 2.6rem;
   }
 `;
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ const IndexPage = () => {
       <Section
         title="Details"
         text="This is some basic starter text for a paragraph. Let us tell you a long winded story about us that you'll never actually read because you're only here for basic information about the wedding. Or perhaps you're a stalker trying to find out where one of us lives so you can watch us from your van."
-      />
+      ></Section>
       <Section title="RSVP" text="RSVP Here!" />
       <Section title="Registry" text="Look at our Registries" />
     </>
