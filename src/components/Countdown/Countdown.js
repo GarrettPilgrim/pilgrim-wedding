4ba33f1f98fa9ebe90be0b7fd4d85dfaab256244
Garrett Pilgrim <pilgrimgarrett@gmail.com>
2021-08-30 16:00:03 -0500

    Add flower svgs and included gatbsy svg

--- src/components/Countdown/Countdown.js
+++ src/components/Countdown/Countdown.js
@@ -24,7 +24,7 @@ const Countdown = () => {
 
   return (
     <CountdownStyle>
-      <Section title="Countdown" />
+      <Section title="" />
       <div className="flex">
         <div>
           <h3>Days</h3>
