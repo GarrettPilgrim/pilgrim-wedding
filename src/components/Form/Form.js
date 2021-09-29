3b6d5b005d0f8798b47446bae84a6ad8eb87cb30
Garrett Pilgrim <pilgrimgarrett@gmail.com>
2021-09-29 14:41:07 -0500

    added hidden input on form with name form-name

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -8,12 +8,13 @@ const Form = () => {
         <fieldset>
           <legend>RSVP</legend>
           <form
-            name="wedding-rsvp"
-            method="post"
+            name="Wedding RSVP"
+            method="POST"
             data-netlify="true"
             netlify-honeypot="bot-field"
             action="/success"
           >
+            <input type="hidden" name="form-name" value="Wedding RSVP" />
             <label>
               First Name
               <input type="text" name="first-name" id="first-name" required />
